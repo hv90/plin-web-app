605027f759d1bc0b2524fb8d0fd6432d2b4b7f9f
hv90 <hugo.machado@hi.com.br>
2021-06-10 22:26:49 -0300

    implement searchbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,17 +2,35 @@ import React, { useState } from 'react';
 
 import logo from '../../assets/images/logo.svg';
 
-import { AccountArea, AccountInfo, HeaderContainer, LogoArea } from './styles';
+import {
+  AccountArea,
+  AccountInfo,
+  BalanceArea,
+  HeaderContainer,
+  LogoArea,
+  SearchBar,
+  SearchBarArea,
+} from './styles';
 
 import Divider from './Divider';
 
 import caretDown from '../../assets/images/icons/caretDown.png';
+import copy from '../../assets/images/icons/copy.png';
+import shield from '../../assets/images/icons/shield.png';
+import eye from '../../assets/images/icons/eye.png';
+import notEye from '../../assets/images/icons/notEye.png';
+import magnifyingGlass from '../../assets/images/icons/magnifyingGlass.png';
 
 const Header: React.FC = () => {
   const [isClicked, setIsClicked] = useState(false);
+  const [shouldShowBalance, setShouldShowBalance] = useState(false);
 
   const dropDownHandler = () => {
     setIsClicked(current => !current);
+  };
+
+  const showBalanceHandler = () => {
+    setShouldShowBalance(current => !current);
   };
 
   return (
@@ -29,9 +47,75 @@ const Header: React.FC = () => {
         </button>
         <AccountInfo>
           <p>Ag. 00005-1</p>
-          <p>C.C. 00000000000000000020-1</p>
+          <p style={{ marginLeft: 15 }}>C.C. 00000000000000000020-1</p>
         </AccountInfo>
       </AccountArea>
+      <img
+        src={copy}
+        alt="copy"
+        style={{ marginTop: 28, right: 20, width: 16, height: 16 }}
+      />
+      <BalanceArea>
+        <button type="button" onClick={showBalanceHandler}>
+          {!shouldShowBalance && (
+            <>
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  marginLeft: 15,
+                }}
+              >
+                <img
+                  src={shield}
+                  alt="shield"
+                  style={{ width: 24, height: 24 }}
+                />
+                <p>Saldo</p>
+              </div>
+              <img src={eye} alt="eye" style={{ width: 24, height: 24 }} />
+            </>
+          )}
+          {shouldShowBalance && (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                marginTop: 10,
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  marginLeft: 20,
+                }}
+              >
+                <p id="show">Saldo</p>
+                <img
+                  src={notEye}
+                  alt="closed eye"
+                  style={{ width: 16, height: 16, marginLeft: 10 }}
+                />
+              </div>
+              <p id="balance">R$40.000,00</p>
+            </div>
+          )}
+        </button>
+      </BalanceArea>
+      <SearchBarArea>
+        <img
+          src={magnifyingGlass}
+          alt="magnifying glass"
+          style={{
+            width: 16,
+            height: 16,
+            position: 'absolute',
+            marginLeft: 134,
+          }}
+        />
+        <SearchBar />
+      </SearchBarArea>
     </HeaderContainer>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -42,6 +42,7 @@ export const AccountArea = styled.div`
     cursor: pointer;
     display: flex;
     align-items: center;
+    padding-left: 0;
   }
 `;
 
@@ -57,4 +58,61 @@ export const AccountInfo = styled.div`
     line-height: 14px;
     margin-top: 5px;
   }
+`;
+
+export const BalanceArea = styled.div`
+  width: 280px;
+
+  button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0;
+  }
+
+  p {
+    font-family: LatoRegular;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 19px;
+    color: #622ee5;
+    margin-left: 10px;
+  }
+
+  #show {
+    font-family: LatoRegular;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    color: #172765;
+    margin: 0;
+  }
+
+  #balance {
+    font-family: LatoRegular;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 19px;
+    color: #69d531;
+    margin: 0 0 0 20px;
+  }
+`;
+
+export const SearchBarArea = styled.div`
+  width: 370px;
+  height: 100%;
+  margin-left: 10px;
+  display: flex;
+  align-items: center;
+`;
+
+export const SearchBar = styled.input`
+  width: 160px;
+  height: 30px;
+  border-radius: 5px;
+  border: 0.5px solid #778899;
 `;
